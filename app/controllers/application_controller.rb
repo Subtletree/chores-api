42dfe6c6001d99a25170c0cf8c2323aee9e67c40
Ryan Scott <ryanscott161@gmail.com>
2016-03-04 15:03:55 +1300

    Use HTTP_REAL_IP for ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::API
   # Return Unauthorized for Ip's not on the whitelist
   def ip_whitelist
     whitelist = ['203.97.99.8']
-    p request
-    if Rails.env.production? && whitelist.exclude?(request.remote_ip)
+    if Rails.env.production? && whitelist.exclude?(request.env['HTTP_REAL_IP'])
       render status: 401
     end
   end
