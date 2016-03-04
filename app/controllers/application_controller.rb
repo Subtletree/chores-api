fade52d9383e06887461f10dab779f582c598ab8
Ryan Scott <ryanscott161@gmail.com>
2016-03-04 14:09:20 +1300

    production filter for ip whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::API
   # Return Unauthorized for Ip's not on the whitelist
   def ip_whitelist
     whitelist = ['203.97.99.8']
-  #  if Rails.env.production? && whitelist.exclude?(request.remote_ip)
-    if whitelist.exclude?(request.remote_ip)
+    p request.remote_id
+    if Rails.env.production? && whitelist.exclude?(request.remote_ip)
       render status: 401
     end
   end
