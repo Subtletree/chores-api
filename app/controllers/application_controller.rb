772be1a50e575d7f244d495185d6603eea53b1e9
Ryan Scott <ryanscott161@gmail.com>
2016-03-04 14:20:49 +1300

    p request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   # Return Unauthorized for Ip's not on the whitelist
   def ip_whitelist
     whitelist = ['203.97.99.8']
-    p request.remote_id
+    p request
     if Rails.env.production? && whitelist.exclude?(request.remote_ip)
       render status: 401
     end
